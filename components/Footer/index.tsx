39ed0506e6d7b0ef470ebf31a083eb5b90dd4103
João Victor Corado <jhonisilva545@gmail.com>
2023-06-24 14:10:07 -0300

    footer bg

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="flex items-center justify-center p-6 lg:px-52 lg:py-10 bg-animado">
+    <div className="flex items-center justify-center p-6 lg:px-52 lg:py-10 text-white">
       <h1>
         Desenvolvido por{" "}
         <a href="http://" target="_blank" rel="noopener noreferrer">
